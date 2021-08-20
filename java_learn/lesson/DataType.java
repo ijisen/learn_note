0a8aa4ec30c1adc4e345f03bc961f8080b8768ce
unknown <471535104@163.com>
2021-08-20 13:46:22 +0800

    modified code

---
+++ java_learn/lesson/DataType.java
@@ -0,0 +1,22 @@
+/**
+ * 数据类型
+ *  申明不同的数据类型，分配的空间不一样
+ *  基本数据类型【原始数据类型】
+ *      第一类： Integer【整数型】
+ * ------- 数据类型 ------- 占用空间大小【单位：子节】
+ *          byte               1
+ *          short              2
+ *          int                4
+ *          long               8
+ *      第二类： Float【浮点型】
+ *          float              4
+ *          double             8
+ *      第三类： Boolean【布尔型】
+ *          boolean            1
+ *      第四类： String【字符型】
+ *          char               2
+ *  引用数据类型
+ *
+ * */
+public class DataType {
+}
