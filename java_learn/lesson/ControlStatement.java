75c76c2b75b2f3e603bf132c4364bc655dd574f1
ijisen <471535104@163.com>
2021-08-31 14:05:15 +0800

    modified code

--- java_learn/lesson/ControlStatement.java
+++ java_learn/lesson/ControlStatement.java
@@ -86,9 +86,71 @@ class ControlStatementFor {
         for (int i = 0; i < index; i++) {
             //  if (item > 5) break;
             // if (item > 5) continue;
-             if (i > 5) return;
+            if (i > 5) return;
             System.out.println(i);
         }
 
+    }
+}
+
+class ControlStatementForDemo1 {
+    public static void main(String[] args) {
+        for (int y = 1; y < 10; y++) {
+            String str = "";
+            for (int x = 1; x <= y; x++) {
+                int result = x * y;
+                str = x + "*" + y + "=" + result;
+                if (result < 10) {
+                    str += "  ";
+                } else {
+                    str += " ";
+                }
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+class ControlStatementForDemo2 {
+    public static void main(String[] args) {
+        int rows = 0;
+        String str = "";
+        for (int i = 1; i <= 1000; i++) {
+            int count = 0;
+            for (int k = 1; k <= i; k++) {
+                // 素数，只能被自己和1整除。
+//                System.out.println(i + "%" + k + "=" + (i % k));
+                if ((i % k) == 0) {
+                    count++;
+                    if (count > 2) {
+                        break;
+                    }
+                }
+            }
+            if (count <= 2) {
+                if (rows == 8) {
+                    rows = 0;
+                    System.out.println(str);
+                    str = "";
+                } else {
+                    rows++;
+                    str = str + i;
+                    str += " ";
+                }
+            }
+        }
+    }
+}
+
+/**
+ * while 控制语句
+ */
+class ControlStatementWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
     }
 }
