cfb798375e9944a224fb1ec4b559c45df608a24a
ijisen <471535104@163.com>
2021-09-07 21:19:19 +0800

    modified code

---
+++ java_learn/lesson/method/Method.java
@@ -0,0 +1,15 @@
+/**
+ * 方法
+ */
+public class Method {
+    public static void main(String[] args) {
+        sumInt(10, 20);
+        sumInt(20, 20);
+    }
+
+    public static void sumInt(int a, int b) {
+        int c = a + b;
+        System.out.println(a + " + " + b + " = " + c);
+    }
+}
+
