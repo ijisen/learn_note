58de762d440b74802ab4d6279c245077f761af6a
ijisen <471535104@163.com>
2021-09-07 21:21:14 +0800

    modified code

--- java_learn/lesson/method/Method.java
+++ java_learn/lesson/method/Method.java
@@ -1,5 +1,6 @@
 /**
- * 方法
+ * java 中叫方法， 不叫函数
+ * 方法必须定义在类当中， 方法定义顺序没有先后顺序
  */
 public class Method {
     public static void main(String[] args) {
@@ -7,6 +8,7 @@ public class Method {
         sumInt(20, 20);
     }
 
+    // 求和方法
     public static void sumInt(int a, int b) {
         int c = a + b;
         System.out.println(a + " + " + b + " = " + c);
