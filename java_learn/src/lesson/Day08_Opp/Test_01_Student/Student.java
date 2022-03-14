dafc52220cd8cb790f016b1fb04ef596b03f7ead
husen <husen@zdns.cn>
2022-03-14 17:11:04 +0800

    learn note

--- java_learn/src/lesson/Day08_Opp/Test_01_Student/Student.java
+++ java_learn/src/lesson/Day08_Opp/Test_01_Student/Student.java
@@ -9,14 +9,31 @@ public class Student {
      * 属性通常采用变量的方式来定义
      * 在方法体呢人，这下属性称为 成员变量
      */
-    // 学号
+    /**
+     * 学号
+     * int 是一种基本数据类型： 整数型
+     * no 是一种【实例变量】
+     * */
     int no;
-    // name
+    /**
+     * name
+     * String 是一种引用数据类型： 代表字符串
+     * name 是一种【引用实例变量】
+     * */
     String name;
-    // gender
+    /**
+     * boolean 是一种基本数据类型： Boolean
+     * gender 是一种【实例变量】
+     * */
     boolean gender;
-    // age
+    /**
+     * int 是一种基本数据类型： 整数型
+     * age 是一种【实例变量】
+     * */
     int age;
-    // address
+    /**
+     * Addr 是一种引用类数据型： 代表家庭住址
+     * addr 是一种【引用实例变量】
+     * */
     Addr addr;
 }
