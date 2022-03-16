96853d4f959eaee5ba6c73638acffd4a34f399ec
husen <husen@zdns.cn>
2022-03-16 08:39:27 +0800

    learn note

---
+++ java_learn/src/lesson/Day08_Opp/Test_06_Main/User_House.java
@@ -0,0 +1,18 @@
+package lesson.Day08_Opp.Test_06_Main;
+/**
+ * 创建一个 house 对象
+ */
+public class User_House {
+    /**
+     * 面积
+     * double: 是一种基本数据类型： 浮点型
+     * area: 是一种【实例变量】
+     * */
+    double area;
+    /**
+     * 户主信息
+     * String: 是一种引用数据类型： 字符串
+     * license: 是一种【引用实例变量】
+     */
+    User user;
+}
