3bf09307e11f223050f498325b6613bb1825ef3f
husen <husen@zdns.cn>
2023-09-20 08:53:08 +0800

    java 学习笔记

--- java_learn/src/lesson/Day04_027_Class_Polymorphic/Test_02_Animal/Cat.java
+++ java_learn/src/lesson/Day04_027_Class_Polymorphic/Test_02_Animal/Cat.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:java_learn/src/lesson/Day04_027_Class_Polymorphic/Test_02_Animal/Cat.java
 package lesson.Day04_027_Class_Polymorphic.Test_02_Animal;
-========
-package Basic.Day04_027_Class_Polymorphic;
->>>>>>>> 6782f9ae50901231290f2af3e0f83b2b54b1f3c8:java_learn/src/Basic/Day04_027_Class_Polymorphic/Cat.java
 
 public class Cat extends Animal {
     String name;
--- java_learn/src/lesson/Day04_027_Class_Polymorphic/Test_03_Animal/Cat.java
+++ java_learn/src/lesson/Day04_027_Class_Polymorphic/Test_03_Animal/Cat.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:java_learn/src/lesson/Day04_027_Class_Polymorphic/Test_03_Animal/Cat.java
 package lesson.Day04_027_Class_Polymorphic.Test_03_Animal;
-========
-package Basic.Day04_027_Class_Polymorphic.demo;
->>>>>>>> 6782f9ae50901231290f2af3e0f83b2b54b1f3c8:java_learn/src/Basic/Day04_027_Class_Polymorphic/demo/Cat.java
 
 /**
  * 创建一个Cat对象， 继承Pet
